e0de4cfcdafc8417dffe44e994e7529270f44925
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-05-03 18:43:28 +0700

    Creat Class Point and Docstring Method

--- C2/first_class.py
+++ C2/first_class.py
@@ -1,4 +1,27 @@
-class MyFirstClass:
-    pass
+# Initialize Point Class
+class Point:
+    def reset(self):
+        self.x = 0
+        self.y = 0
 
-class Point
+p = Point()
+
+Point.reset(p)
+
+# p.reset()
+
+print(p.x, p.y)
+
+# Instances of Class Point
+p1 = Point()
+p2 = Point()
+
+# Add attributes to instance
+p1.x = 5
+p1.y = 10
+
+p2.x = 10
+p2.y = 20
+
+print(p1.x, p1.y)
+print(p2.x, p2.y)
